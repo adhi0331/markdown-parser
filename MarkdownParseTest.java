e98c6c8252343dee2f3f0a3028c4a5fa9d9bcee6
adhi0331 <adhi03.ananthan@gmail.com>
2022-04-21 19:11:13 -0700

    Create MarkdownParseTest.java

---
+++ MarkdownParseTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import org.junit.*;
+public class MarkdownParseTest {
+    @Test
+    public void getLinkTest() throws IOException {
+        ArrayList<String> answers = new ArrayList<>();
+        answers.add("https://something.com");
+        answers.add("some-thing.html");
+        Path fileName = Path.of("myFile1.md");
+        String content = Files.readString(fileName);
+        assertEquals(answers.get(0), MarkdownParse.getLinks(content).get(0)); 
+        assertEquals(answers.get(1), MarkdownParse.getLinks(content).get(1));
+
+    }
+}
